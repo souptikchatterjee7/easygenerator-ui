30baa06838443da3b12bc0faff241aaaafc867b3
souptik-edninja <souptik@edninja.in>
2024-06-15 21:57:31 +0530

    MOre code fixes

--- src/store/actions/userThunks.js
+++ src/store/actions/userThunks.js
@@ -1,11 +1,15 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { API_URL } from "../../config";
 
 export const loginUser = createAsyncThunk(
     "users/login",
     async (userData, { rejectWithValue }) => {
         try {
-            const response = await axios.post("/users/login", userData);
+            const response = await axios.post(
+                API_URL + "/users/login",
+                userData
+            );
             console.log(response);
             return response.data;
         } catch (error) {
@@ -19,7 +23,10 @@ export const registerUser = createAsyncThunk(
     "users/register",
     async (userData, { rejectWithValue }) => {
         try {
-            const response = await axios.post("/users/register", userData);
+            const response = await axios.post(
+                API_URL + "/users/register",
+                userData
+            );
             console.log(response);
             return response.data;
         } catch (error) {
@@ -33,7 +40,7 @@ export const getUserProfile = createAsyncThunk(
     "users/profile",
     async (_, { rejectWithValue }) => {
         try {
-            const response = await axios.get("/users/profile");
+            const response = await axios.get(API_URL + "/users/profile");
             console.log(response);
             return response.data;
         } catch (error) {
--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -11,53 +11,53 @@ const initialState = {
 
 const userReducer = createReducer(initialState, (builder) => {
     builder
-        .addCase(loginUser.pending, (state) => {
-            state.isLoading = true;
-            state.error = null;
-        })
-        .addCase(loginUser.fulfilled, (state, action) => {
-            console.log(action);
-            state.isLoading = false;
-            state.isAuthenticated = true;
-            state.authenticationToken = action.payload.token;
-            state.user = action.payload.user;
-        })
-        .addCase(loginUser.rejected, (state, action) => {
-            console.log(action);
-            state.isLoading = false;
-            state.error = action.payload.message;
-        })
-        .addCase(registerUser.pending, (state) => {
-            state.isLoading = true;
-            state.error = null;
-        })
-        .addCase(registerUser.fulfilled, (state, action) => {
-            console.log(action);
-            state.isLoading = false;
-            state.isAuthenticated = true;
-            state.authenticationToken = action.payload.token;
-            state.user = action.payload.user;
-        })
-        .addCase(registerUser.rejected, (state, action) => {
-            console.log(action);
-            state.isLoading = false;
-            state.error = action.payload.message;
-        })
-        .addCase(getUserProfile.pending, (state) => {
-            state.isLoading = true;
-            state.error = null;
-        })
-        .addCase(getUserProfile.fulfilled, (state, action) => {
-            console.log(action);
-            state.isLoading = false;
-            state.isAuthenticated = true;
-            state.user = action.payload.user;
-        })
-        .addCase(getUserProfile.rejected, (state, action) => {
-            console.log(action);
-            state.isLoading = false;
-            state.error = action.payload.message;
-        });
+        .addMatcher(
+            (action) =>
+                [
+                    loginUser.pending,
+                    registerUser.pending,
+                    getUserProfile.pending
+                ].includes(action.type),
+            (state) => {
+                state.isLoading = true;
+                state.error = null;
+            }
+        )
+        .addMatcher(
+            (action) =>
+                [loginUser.fulfilled, registerUser.fulfilled].includes(
+                    action.type
+                ),
+            (state, action) => {
+                console.log(action);
+                state.isLoading = false;
+                state.isAuthenticated = true;
+                state.authenticationToken = action.payload.token;
+                state.user = action.payload.user;
+            }
+        )
+        .addMatcher(
+            (action) =>
+                [
+                    loginUser.rejected,
+                    registerUser.rejected,
+                    getUserProfile.rejected
+                ].includes(action.type),
+            (state, action) => {
+                console.log(action);
+                state.isLoading = false;
+                state.error = action.payload.message;
+            }
+        )
+        .addMatcher(
+            (action) => getUserProfile.fulfilled(action),
+            (state, action) => {
+                console.log(action);
+                state.isLoading = false;
+                state.isAuthenticated = true;
+                state.user = action.payload.user;
+            }
+        );
 });
 
 export default userReducer;
