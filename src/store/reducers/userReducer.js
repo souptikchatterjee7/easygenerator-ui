795f8edf77b1c0de954b9f8e5c333611bed76509
souptik-edninja <souptik@edninja.in>
2024-06-15 20:52:56 +0530

    fixes

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -12,42 +12,52 @@ const initialState = {
 const userReducer = createReducer(initialState, (builder) => {
     builder
         .addCase(loginUser.pending, (state) => {
+            console.log(action);
             state.loading = true;
             state.error = null;
         })
         .addCase(loginUser.fulfilled, (state, action) => {
+            console.log(action);
             state.loading = false;
-            state.isAuthenticated = false;
-            state.authenticationToken = action.token;
-            state.user = null;
+            state.isAuthenticated = true;
+            state.authenticationToken = action.payload.token;
+            state.user = action.payload.user;
         })
         .addCase(loginUser.rejected, (state, action) => {
+            console.log(action);
             state.loading = false;
             state.error = action.payload.message;
         })
         .addCase(registerUser.pending, (state) => {
+            console.log(action);
             state.loading = true;
             state.error = null;
         })
         .addCase(registerUser.fulfilled, (state) => {
+            console.log(action);
             state.loading = false;
-            state.isAuthenticated = false;
-            state.user = null;
+            state.isAuthenticated = true;
+            state.authenticationToken = action.payload.token;
+            state.user = action.payload.user;
         })
         .addCase(registerUser.rejected, (state, action) => {
+            console.log(action);
             state.loading = false;
             state.error = action.payload.message;
         })
         .addCase(getUserProfile.pending, (state) => {
+            console.log(action);
             state.loading = true;
             state.error = null;
         })
         .addCase(getUserProfile.fulfilled, (state) => {
+            console.log(action);
             state.loading = false;
             state.isAuthenticated = true;
-            state.user = action.payload;
+            state.user = action.payload.user;
         })
         .addCase(getUserProfile.rejected, (state, action) => {
+            console.log(action);
             state.loading = false;
             state.error = action.payload.message;
         });
