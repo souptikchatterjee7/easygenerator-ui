51082a76a0595f9f960af538aee35802a1840926
souptik-edninja <souptik@edninja.in>
2024-06-15 20:01:13 +0530

    Fixes

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,14 +1,11 @@
 import axios from "axios";
-const environment = process.argv[2] || "dev";
-import { config } from "../config";
-const envConfig = config[environment];
 
 export const signup = (data) => async (dispatch) => {
     try {
         dispatch({ type: "USER_SIGNUP_REQUEST" });
 
         const response = await axios.post(
-            envConfig.API_URL + "/users/register",
+            process.env.API_URL + "/users/register",
             data
         );
 
@@ -23,7 +20,7 @@ export const login = (data) => async (dispatch) => {
         dispatch({ type: "USER_LOGIN_REQUEST" });
 
         const response = await axios.post(
-            envConfig.API_URL + "/users/login",
+            process.env.API_URL + "/users/login",
             data
         );
 
@@ -37,7 +34,9 @@ export const getProfile = () => async (dispatch) => {
     try {
         dispatch({ type: "USER_GET_PROFILE_REQUEST" });
 
-        const response = await axios.get(envConfig.API_URL + "/users/profile");
+        const response = await axios.get(
+            process.env.API_URL + "/users/profile"
+        );
 
         dispatch({ type: "USER_GET_PROFILE_SUCCESS", payload: response.data });
     } catch (error) {
@@ -49,7 +48,7 @@ export const logout = () => async (dispatch) => {
     try {
         dispatch({ type: "USER_LOGOUT_REQUEST" });
 
-        const response = await axios.get(envConfig.API_URL + "/users/logout");
+        const response = await axios.get(process.env.API_URL + "/users/logout");
 
         dispatch({ type: "USER_LOGOUT_SUCCESS", payload: response.data });
     } catch (error) {
